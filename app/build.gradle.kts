9ddd6d618e9d8490be60dbb66c011858b0dc4b7b
gibranrafii <gibranrafi21@gmail.com>
2025-11-21 13:19:21 +0700

    membenarkan typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
